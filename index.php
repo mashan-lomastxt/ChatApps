3e6ee9f6b000953379d25d091d7a7493b28a6d90
mashan-lomastxt <riska.handika@students.amikom.ac.id>
2021-03-19 20:38:59 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php 
+  session_start();
+  if(isset($_SESSION['unique_id'])){
+    header("location: users.php");
+  }
+?>
+
+<?php include_once "header.php"; ?>
+<body>
+  <div class="wrapper">
+    <section class="form signup">
+      <header>Realtime Chat App</header>
+      <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
+        <div class="error-text"></div>
+        <div class="name-details">
+          <div class="field input">
+            <label>Nama Depan</label>
+            <input type="text" name="fname" placeholder="Nama Depan" required>
+          </div>
+          <div class="field input">
+            <label>Nama Belakang</label>
+            <input type="text" name="lname" placeholder="Nama Belakang" required>
+          </div>
+        </div>
+        <div class="field input">
+          <label>Alamat Email</label>
+          <input type="text" name="email" placeholder="Masukkan Email" required>
+        </div>
+        <div class="field input">
+          <label>Password</label>
+          <input type="password" name="password" placeholder="password" required>
+          <i class="fas fa-eye"></i>
+        </div>
+        <div class="field image">
+          <label>Pilih Foto</label>
+          <input type="file" name="image" accept="image/x-png,image/gif,image/jpeg,image/jpg" required>
+        </div>
+        <div class="field button">
+          <input type="submit" name="submit" value="Mulai untuk chat">
+        </div>
+      </form>
+      <div class="link">Sudah punya Akun..? <a href="login.php">Login Sekarang</a></div>
+    </section>
+  </div>
+
+  <script src="javascript/pass-show-hide.js"></script>
+  <script src="javascript/signup.js"></script>
+
+</body>
+</html>
